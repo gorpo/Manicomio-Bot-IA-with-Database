b6bf1e51334452a63451511b75cc15540243ea46
gorpoorko <gorpoorko@protonmail.com>
2020-08-11 18:33:05 -0300

    versao compactada com menos plugins

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -149,7 +149,7 @@ Se esse erro persistir entre em contato com @GorpoOrko.'''.format(e.description)
 📅Permanência: {dias}""")
                             cursor_sqlite.execute(f"""INSERT INTO permanencia(int_id,grupo,id_grupo, admin, doador, id_doador, dias, data_inicial, data_final,data_aviso)VALUES(null,'{msg['chat']['title']}','{msg['chat']['id']}','{admin}','{doador}','{id_doador}','{dias}','{data_inicial}','{data_final}','{data_aviso}')""")
                             conexao_sqlite.commit()
-                            
+
                             #print(admin, doador, id_doador, dias, data_inicial, data_final)
                             try:#PEGA A FOTO DO USUARIO E ENVIA NO Grupo
                                 a = await bot.getUserProfilePhotos(msg['new_chat_member']['id'])
@@ -158,7 +158,7 @@ Se esse erro persistir entre em contato com @GorpoOrko.'''.format(e.description)
                             except Exception as e:
                                 pass
             except Exception as e:
-                await bot.sendMessage(chat_id,f"🤖 `Ocorreu um erro ao inserir os dados na Database.Envie novamente o comando manualmente conforme exemplo:` ```restringir @usuario id_usuario dias``` ***Exemplo:*** restringir @xbadcoffee 1367451130 30 ",'markdown')
+                await bot.sendMessage(chat_id,f"🤖{e} `Ocorreu um erro ao inserir os dados na Database.Envie novamente o comando manualmente conforme exemplo:` ```restringir @usuario id_usuario dias``` ***Exemplo:*** restringir @xbadcoffee 1367451130 30 ",'markdown')
             ###########FIM DO SISTEMA DE BANIMENTO---------------------------------------------------------------------------
             #ACIMA TUDO CODIGO MEU------------------------->
 
